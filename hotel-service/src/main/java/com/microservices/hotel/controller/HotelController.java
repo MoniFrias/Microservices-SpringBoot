370f27339fde676a585e35cdaadd4b16dbe9c431
MoniFrias <friasmoni18@gmail.com>
2023-10-02 14:36:43 -0600

    Circuitbreaker with Resilienc4J

--- hotel-service/src/main/java/com/microservices/hotel/controller/HotelController.java
+++ hotel-service/src/main/java/com/microservices/hotel/controller/HotelController.java
@@ -3,6 +3,8 @@ package com.microservices.hotel.controller;
 import java.util.List;
 import java.util.Objects;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +26,8 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 public class HotelController {
 	
 	private final HotelService hotelService;
+	
+	 private Logger logger = LoggerFactory.getLogger(HotelController.class);
 	
 	@Autowired
 	public HotelController (HotelService hotelService) {
@@ -61,9 +65,9 @@ public class HotelController {
 		return hotels.isEmpty() ? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(hotels, HttpStatus.OK);
 	}
 	
-	
-	private ResponseEntity<List<Rating>> fallBackGetRatings(@PathVariable final Long hotelId, RuntimeException ex){
-		return new ResponseEntity("Hotel : " + hotelId + " doesn't have ratings", HttpStatus.OK);
+	private ResponseEntity<List<Rating>> fallBackGetRatings(RuntimeException ex){
+		logger.info("Fallback is executed because service is down ", ex.getMessage());
+		return new ResponseEntity("Oops! Something went wrong, please try again later!", HttpStatus.OK);
 	}
 	
 }
--- user-service/src/main/java/com/microservices/user/controller/UserController.java
+++ user-service/src/main/java/com/microservices/user/controller/UserController.java
@@ -3,6 +3,8 @@ package com.microservices.user.controller;
 import java.util.List;
 import java.util.Objects;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +19,16 @@ import com.microservices.user.entity.User;
 import com.microservices.user.model.Rating;
 import com.microservices.user.service.UserService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping("/user")
-public class userController {
+public class UserController {
 	
 	@Autowired
 	private UserService userService;
+	
+	private Logger logger = LoggerFactory.getLogger(UserController.class);
 	
 	@PostMapping(path = "/saveUser")
 	public ResponseEntity<User> saveUser(@RequestBody User user){
@@ -40,6 +46,7 @@ public class userController {
 	}
 	
 	@GetMapping(path = "/getRatings/{userId}")
+	@CircuitBreaker(name = "userCB", fallbackMethod = "fallBackGetRatings")
 	public ResponseEntity<List<Rating>> getRatings(@PathVariable final Long userId){
 		List<Rating> ratings = userService.getRatings(userId);
 		if(ratings.isEmpty()) {
@@ -55,6 +62,11 @@ public class userController {
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		}
 		return new ResponseEntity<>(users, HttpStatus.OK);
+	}
+	
+	private ResponseEntity<List<Rating>> fallBackGetRatings(RuntimeException ex){
+		logger.info("Fallback is executed because service is down ", ex.getMessage());
+		return new ResponseEntity("Oops! Something went wrong, please try again later!", HttpStatus.OK);
 	}
 	
 }
