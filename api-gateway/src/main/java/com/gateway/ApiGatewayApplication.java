0838afdf1ab212bd36d68ef0519f3a95adf75aa8
MoniFrias <friasmoni18@gmail.com>
2023-10-02 13:13:59 -0600

    Implement API Gateway

--- api-gateway/src/main/java/com/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/gateway/ApiGatewayApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableEurekaClient is deprecated, no need to annotate. It is enough to add the
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
